5b36cae3c8f7ab5830d8325705a325f570b07054
Schipper,Mario M.A <mario.schipper@student.fontys.nl>
2016-12-01 12:35:36 +0100

    AuctionMgr fix volgens opdracht

---
+++ assignment 3 - BUSY/src/main/java/auction/dao/ItemDAOJPAImpl.java
@@ -0,0 +1,58 @@
+package auction.dao;
+
+import auction.domain.Item;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class ItemDAOJPAImpl implements ItemDAO {
+    EntityManagerFactory ef = Persistence.createEntityManagerFactory("db");
+    EntityManager items = ef.createEntityManager();
+    
+    public ItemDAOJPAImpl() {
+    }
+
+    @Override
+    public int count() {
+        return (Integer) items.createNativeQuery("SELECT count(*) FROM item")
+                .getSingleResult();  
+    }
+
+    @Override
+    public void create(Item item) {
+        items.getTransaction().begin();
+        items.persist(item);
+        items.getTransaction().commit();
+    }
+
+    @Override
+    public void edit(Item item) {
+        items.merge(item);
+    }
+
+
+    @Override
+    public List<Item> findAll() {
+        return items.createQuery("SELECT * FROM Item").getResultList();
+    }
+
+    @Override
+    public void remove(Item item) {
+        items.getTransaction().begin();
+        items.remove(this.find(item.getId()));
+        items.getTransaction().commit();
+    }
+
+    @Override
+    public Item find(Long id) {
+        return items.find(Item.class, id);
+    }
+
+    @Override
+    public List<Item> findByDescription(String description) {
+        Query q = items.createQuery("SELECT item FROM ITEM WHERE item.description = :description", Item.class); //HMMMM??? effe navragen aan ROBINIO
+        return q.setParameter("description", description).getResultList();
+    }
+}
--- assignment 3 - BUSY/src/main/java/auction/service/AuctionMgr.java
+++ assignment 3 - BUSY/src/main/java/auction/service/AuctionMgr.java
@@ -1,5 +1,7 @@
 package auction.service;
 
+import auction.dao.ItemDAO;
+import auction.dao.ItemDAOJPAImpl;
 import nl.fontys.util.Money;
 import auction.domain.Bid;
 import auction.domain.Item;
@@ -7,26 +9,29 @@ import auction.domain.User;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AuctionMgr  {
+public class AuctionMgr {
 
-   /**
-     * @param id
-     * @return het item met deze id; als dit item niet bekend is wordt er null
-     *         geretourneerd
-     */
-    public Item getItem(Long id) {
-        // TODO
-        return null;
+    private ItemDAO itemDAO;
+
+    public AuctionMgr() {
+        itemDAO = new ItemDAOJPAImpl();
     }
 
-  
-   /**
+    /**
+     * @param id
+     * @return het item met deze id; als dit item niet bekend is wordt er null
+     * geretourneerd
+     */
+    public Item getItem(Long id) {
+        return itemDAO.find(id);
+    }
+
+    /**
      * @param description
      * @return een lijst met items met @desciption. Eventueel lege lijst.
      */
     public List<Item> findItemByDescription(String description) {
-        // TODO
-        return new ArrayList<Item>();
+        return itemDAO.findByDescription(description);
     }
 
     /**
@@ -34,10 +39,9 @@ public class AuctionMgr  {
      * @param buyer
      * @param amount
      * @return het nieuwe bod ter hoogte van amount op item door buyer, tenzij
-     *         amount niet hoger was dan het laatste bod, dan null
+     * amount niet hoger was dan het laatste bod, dan null
      */
     public Bid newBid(Item item, User buyer, Money amount) {
-        // TODO 
-        return null;
+        return item.newBid(buyer, amount);
     }
 }
