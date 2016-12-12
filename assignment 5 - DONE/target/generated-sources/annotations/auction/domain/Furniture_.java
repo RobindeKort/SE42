f07cfb4154353b679c7aaf75ff153c762a99ec4f
Schipper,Mario M.A <mario.schipper@student.fontys.nl>
2016-12-12 18:22:50 +0100

    DONE

---
+++ assignment 5 - DONE/target/generated-sources/annotations/auction/domain/Furniture_.java
@@ -0,0 +1,13 @@
+package auction.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-12T17:53:28")
+@StaticMetamodel(Furniture.class)
+public class Furniture_ extends Item_ {
+
+    public static volatile SingularAttribute<Furniture, String> madeFrom;
+
+}
---
+++ assignment 5 - DONE/target/generated-sources/annotations/auction/domain/User_.java
@@ -0,0 +1,17 @@
+package auction.domain;
+
+import auction.domain.Item;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-12T17:53:28")
+@StaticMetamodel(User.class)
+public class User_ { 
+
+    public static volatile SetAttribute<User, Item> offeredItems;
+    public static volatile SingularAttribute<User, Long> Id;
+    public static volatile SingularAttribute<User, String> email;
+
+}
